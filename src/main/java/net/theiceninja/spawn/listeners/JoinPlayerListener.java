dbda7b52576023dcf38d32ebee81ea7a18a48874
TheIceNinja <97467325+TheIceNinja@users.noreply.github.com>
2022-06-21 18:43:20 +0300

    Add files via upload

---
+++ src/main/java/net/theiceninja/spawn/listeners/JoinPlayerListener.java
@@ -0,0 +1,28 @@
+package net.theiceninja.spawn.listeners;
+
+import net.theiceninja.spawn.Main;
+import net.theiceninja.spawn.utils.MessageUtils;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinPlayerListener implements Listener {
+    Main plugin;
+
+    public JoinPlayerListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        Location location = plugin.getConfig().getLocation("spawn");
+        if (location != null){
+            p.teleport(location);
+
+        }else{
+        }
+    }
+}
---
+++ src/main/java/net/theiceninja/spawn/listeners/RespawnListener.java
@@ -0,0 +1,28 @@
+package net.theiceninja.spawn.listeners;
+
+import net.theiceninja.spawn.Main;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+public class RespawnListener implements Listener {
+    Main plugin;
+
+    public RespawnListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e){
+        Player p = e.getPlayer();
+        Location location = plugin.getConfig().getLocation("spawn");
+        if (location != null){
+            e.setRespawnLocation(location);
+
+        }else{
+        }
+
+    }
+}
