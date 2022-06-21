6a04377ad97f6fe5f6e2f63d76613fc0f18d9179
TheIceNinja <97467325+TheIceNinja@users.noreply.github.com>
2022-06-21 19:02:22 +0300

    Update MessageUtils.java

--- src/main/java/net/theiceninja/spawn/utils/MessageUtils.java
+++ src/main/java/net/theiceninja/spawn/utils/MessageUtils.java
@@ -5,5 +5,5 @@ public class MessageUtils {
     public static String NO_PERMS = ColorUtils.color("&cסליחה, אבל אין לך הרשאה לפקודה הזאת!");
     public static String PREFIX = ColorUtils.color("&7SpawnPlugin &f|");
     public static String NO_SPAWN = ColorUtils.color(MessageUtils.PREFIX + " &cצריך לעשות /setspawn בשביל שיהיה ספאון למקום שאתה רוצה!");
-    public static String PLAYER_ERR = ColorUtils.color(MessageUtils.PREFIX + " &cשחקן זה לא מחובר/ ");
+    public static String PLAYER_ERR = ColorUtils.color(MessageUtils.PREFIX + " &cשחקן זה לא מחובר/לא קיים ");
 }
