c7b1962bde3e052eaef8ba7f2e1aef622f2ae4a0
TheIceNinja <97467325+TheIceNinja@users.noreply.github.com>
2022-06-21 18:42:36 +0300

    Delete spawn directory

--- spawn/src/main/java/net/theiceninja/spawn/utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package net.theiceninja.spawn.utils;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtils {
-    public static String color(String message){
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-}
--- spawn/src/main/java/net/theiceninja/spawn/utils/MessageUtils.java
+++
@@ -1,9 +0,0 @@
-package net.theiceninja.spawn.utils;
-
-public class MessageUtils {
-    public static String NOT_PLAYER = ColorUtils.color("&cYou cant do that as a console please login and do that command.");
-    public static String NO_PERMS = ColorUtils.color("&cסליחה, אבל אין לך הרשאה לפקודה הזאת!");
-    public static String PREFIX = ColorUtils.color("&7SpawnPlugin &f|");
-    public static String NO_SPAWN = ColorUtils.color(MessageUtils.PREFIX + " &cצריך לעשות /setspawn בשביל שיהיה ספאון למקום שאתה רוצה!");
-    public static String PLAYER_ERR = ColorUtils.color(MessageUtils.PREFIX + " &cשחקן זה לא מחובר/ ");
-}
