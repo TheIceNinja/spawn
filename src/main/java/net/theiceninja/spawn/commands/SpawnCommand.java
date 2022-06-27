3c04f535ac0160c134232e0708c9bd160621a13d
TheIceNinja <97467325+TheIceNinja@users.noreply.github.com>
2022-06-28 00:56:05 +0300

    Update SpawnCommand.java

--- src/main/java/net/theiceninja/spawn/commands/SpawnCommand.java
+++ src/main/java/net/theiceninja/spawn/commands/SpawnCommand.java
@@ -21,14 +21,17 @@ public class SpawnCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player){
             Player p = (Player) sender;
-            if (p.hasPermission("spawn.spawn")){
                 if (args.length == 0) {
-                    Location location = plugin.getConfig().getLocation("spawn");
-                    if (location != null) {
-                        p.teleport(location);
-                        p.sendMessage(ColorUtils.color(MessageUtils.PREFIX + " &aהשתגרת לספאון!"));
-                    } else {
-                        p.sendMessage(ColorUtils.color(MessageUtils.PREFIX + " &cצריך לעשות /setspawn בשביל שיהיה ספאון למקום שאתה רוצה!"));
+                    if (p.hasPermission("spawn.spawn")) {
+                        Location location = plugin.getConfig().getLocation("spawn");
+                        if (location != null) {
+                            p.teleport(location);
+                            p.sendMessage(ColorUtils.color(MessageUtils.PREFIX + " &aהשתגרת לספאון!"));
+                        } else {
+                            p.sendMessage(ColorUtils.color(MessageUtils.PREFIX + " &cצריך לעשות /setspawn בשביל שיהיה ספאון למקום שאתה רוצה!"));
+                        }
+                    }else{
+                        p.sendMessage(MessageUtils.PREFIX + " " + MessageUtils.NO_PERMS);
                     }
                 }else if (args.length == 1) {
                     if (p.hasPermission("spawn.spawn.other")) {
@@ -47,14 +50,13 @@ public class SpawnCommand implements CommandExecutor {
                             p.sendMessage(MessageUtils.NO_SPAWN);
                         }
                     }else{
-                        p.sendMessage(MessageUtils.NO_PERMS);
-                       }
+                        p.sendMessage(MessageUtils.PREFIX + " " + MessageUtils.NO_PERMS);
                     }
+                }
 
 
-            }else{
-                p.sendMessage(MessageUtils.PREFIX + " " + MessageUtils.NO_PERMS);
-            }
+
+
         }else{
             sender.sendMessage(MessageUtils.NOT_PLAYER);
         }
